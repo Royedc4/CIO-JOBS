337c001de99e1467e956a068fc16d6bf56517e2b
roYCalderon <royedc4@gmail.com>
2016-06-14 17:53:45 -0400

    Elimination de respaldo copia y modificación de nombre al original en producción UPDT_ARTS

--- COSER/updt_arts.prg.sql
+++ COSER/updt_arts.prg.sql
@@ -1,14 +1,11 @@
-**********************************************************************************************************
-*** ACTUALIZACION DE ARTICULOS (updt_arts)
-**********************************************************************************************************
-*** Distribuye los datos de la empresa Matriz (COSER_A) a otras empresas
-*** Con el fin de actualizar costos y precios demas datos de articulos.
-*** Actualiza las empresas ART_A CONAI_A y COSEP_A asegurando la siguiente premisa
-*** COSER_A.ART.COS_MERC > EMPRESAXXX.ART.ULT_COS_UN 
-**********************************************************************************************************
-*** Roy Febrero 2016
-**********************************************************************************************************
-
+***********************************************************************************************************
+*** ACTUALIZACION DE ARTICULOS (updt_arts)                                                              ***
+***********************************************************************************************************
+*** Distribuye los datos de la empresa Matriz (COSER_A) a otras empresas                                ***
+*** Con el fin de actualizar costos y precios demas datos de articulos.                                 ***
+*** Actualiza las empresas ART_A CONAI_A y COSEP_A asegurando la siguiente premisa                      ***
+*** COSER_A.ART.COS_MERC > EMPRESAXXX.ART.ULT_COS_UN                                                    ***
+***********************************************************************************************************
 
 tconnect = SQLCONECTAR()
 tconnect1 = SQLCONECTAR()
@@ -33,8 +30,66 @@ Endif
 MESSAGEBOX(":.:Proceso Actualizacion De Precios :.: "+Chr(13)+"-->  INICIADO <--" +Chr(13)+"Este proceso puede tomar al rededor de un minuto.",64,"::Dpto Informatica :) Compresores Servicios::")
 
 *************************************************************************
+*INSERT DE COLOR*
+*************************************************************************
+
+tresult=sqlexec(tconnect,'select * from COSER_A.dbo.colores','v_color')
+
+
+do while !EOF('v_color')
+	*ART_A
+		tresult1=sqlexec(tconnect1,'select * from ART_A.dbo.colores where co_col=?v_color.co_col','v_resultado')
+		if EOF('v_resultado')
+			MESSAGEBOX("ART_A: SE INSERTO UNA NUEVA CATEGORIA:" + v_color.co_col)
+			ins="INSERT INTO [ART_A].[dbo].[colores] (co_col,des_col) VALUES (?v_color.co_col,?v_color.des_col)"
+			tresult2=sqlexec(tconnect2,ins)
+			If mensaje_sql(tresult2,1,"Error sql Insertando Datos EN ART_A, INFORMAR AL DPTO INFORMATICA") <= 0
+			  Return .F.
+			ENDIF
+		ENDIF
+
+	*COSEP_A
+		tresult1=sqlexec(tconnect1,'select * from COSEP_A.dbo.colores where co_col=?v_color.co_col','v_resultado')
+		if EOF('v_resultado')
+			MESSAGEBOX("COSEP_A: SE INSERTO UNA NUEVA CATEGORIA:" + v_color.co_col)
+			ins="INSERT INTO [COSEP_A].[dbo].[colores] (co_col,des_col) VALUES (?v_color.co_col,?v_color.des_col)"
+			tresult2=sqlexec(tconnect2,ins)
+			If mensaje_sql(tresult2,1,"Error sql Insertando Datos EN COSEP_A, INFORMAR AL DPTO INFORMATICA") <= 0
+			  Return .F.
+			ENDIF
+		ENDIF
+
+	*CONAI_A
+		tresult1=sqlexec(tconnect1,'select * from CONAI_A.dbo.colores where co_col=?v_color.co_col','v_resultado')
+		if EOF('v_resultado')
+			MESSAGEBOX("CONAI_A: SE INSERTO UNA NUEVA CATEGORIA:" + v_color.co_col)
+			ins="INSERT INTO [CONAI_A].[dbo].[colores] (co_col,des_col) VALUES (?v_color.co_col,?v_color.des_col)"
+			tresult2=sqlexec(tconnect2,ins)
+			If mensaje_sql(tresult2,1,"Error sql Insertando Datos EN CONAI_A, INFORMAR AL DPTO INFORMATICA") <= 0
+			  Return .F.
+			ENDIF
+		ENDIF
+
+	*FRIOS_A
+		tresult1=sqlexec(tconnect1,'select * from FRIOS_A.dbo.colores where co_col=?v_color.co_col','v_resultado')
+		if EOF('v_resultado')
+			MESSAGEBOX("FRIOS_A: SE INSERTO UNA NUEVA CATEGORIA:" + v_color.co_col)
+			ins="INSERT INTO [FRIOS_A].[dbo].[colores] (co_col,des_col) VALUES (?v_color.co_col,?v_color.des_col)"
+			tresult2=sqlexec(tconnect2,ins)
+			If mensaje_sql(tresult2,1,"Error sql Insertando Datos EN FRIOS_A, INFORMAR AL DPTO INFORMATICA") <= 0
+			  Return .F.
+			ENDIF
+		ENDIF
+
+
+
+skip in v_color
+enddo
+
+*************************************************************************
 *INSERT DE LINEA*
 *************************************************************************
+
 tresult=sqlexec(tconnect,'select * from COSER_A.dbo.lin_art','v_linea')
 
 do while !EOF('v_linea')
@@ -48,7 +103,7 @@ do while !EOF('v_linea')
 			   Return .F.
 			ENDIF
 		ENDIF
-	
+
 	*COSEP_A
 		tresult1=sqlexec(tconnect1,'select * from COSEP_A.dbo.lin_art where co_lin=?v_linea.co_lin','v_resultado')
 		if EOF('v_resultado')
@@ -59,7 +114,7 @@ do while !EOF('v_linea')
 			   Return .F.
 			ENDIF
 		ENDIF
-	
+
 
 	*CONAI_A
 		tresult1=sqlexec(tconnect1,'select * from CONAI_A.dbo.lin_art where co_lin=?v_linea.co_lin','v_resultado')
@@ -82,15 +137,14 @@ do while !EOF('v_linea')
 			   Return .F.
 			ENDIF
 		ENDIF
-	
+
 skip in v_linea
 enddo
-
-
 
 *************************************************************************
 *INSERT DE SUBLINEA*
 *************************************************************************
+
 tresult=sqlexec(tconnect,'select * from COSER_A.dbo.sub_lin','v_sublinea')
 
 do while !EOF('v_sublinea')
@@ -104,7 +158,7 @@ do while !EOF('v_sublinea')
 			 Return .F.
 			ENDIF
 		ENDIF
-	
+
 	*COSEP_A
 		tresult1=sqlexec(tconnect1,'select * from COSEP_A.dbo.sub_lin where co_subl=?v_sublinea.co_subl','v_resultado')
 		if EOF('v_resultado')
@@ -147,8 +201,8 @@ enddo
 *************************************************************************
 *INSERT DE CATEGORIA*
 *************************************************************************
-tresult=sqlexec(tconnect,'select * from COSER_A.dbo.cat_art','v_cat')
 
+tresult=sqlexec(tconnect,'select * from COSER_A.dbo.cat_art','v_cat')
 
 do while !EOF('v_cat')
 	*ART_A
@@ -195,28 +249,26 @@ do while !EOF('v_cat')
 			ENDIF
 		ENDIF
 
-
-
 skip in v_cat
 enddo
 
 *************************************************************************
 *INSERT DE ARTICULOS*
 *************************************************************************
+
 con=0
 conCOSEP=0
 conCONAI=0
 conFRIOS=0
 
-
 tresult=sqlexec(tconnect,'select * from COSER_A.dbo.art','v_SC')
 
-do while !EOF('v_SC')	
+do while !EOF('v_SC')
 
 	*ART_A
 		tresult1=sqlexec(tconnect1,'select * from ART_A.dbo.art where co_art=?v_SC.co_art','v_resultado')
 		if EOF('v_resultado')
-			
+
 			ins="INSERT INTO [ART_A].[dbo].[art](STOCK_ACT, CO_ART,ART_DES,CO_LIN,CO_CAT,CO_SUBL,CO_COLOR,PROCEDENCI,CO_PROV,UNI_VENTA,"+;
 			" SUNI_VENTA,TIPO,TIPO_IMP,COMENTARIO,PORC_COS,cos_merc,prec_vta1,prec_vta2,prec_vta3,prec_vta4,prec_vta5, dis_cen, tipo_cos, campo1, campo2, campo8)"+;
 			" VALUES (?v_SC.stock_act, ?v_SC.co_art,?v_SC.art_des,?v_SC.co_lin,?v_SC.co_cat,?v_SC.co_subl,?v_SC.co_color,?v_SC.procedenci,"+;
@@ -229,14 +281,14 @@ do while !EOF('v_SC')
 			   Return .F.
 			else
 				con=con+1
-				*MESSAGEBOX("ART_A: SE INSERTO UN NUEVO ARTICULO: " + v_SC.co_art)	
+				*MESSAGEBOX("ART_A: SE INSERTO UN NUEVO ARTICULO: " + v_SC.co_art)
 			ENDIF
 		ENDIF
 
 	*COSEP_A
 		tresult1=sqlexec(tconnect1,'select * from COSEP_A.dbo.art where co_art=?v_SC.co_art','v_resultado')
 		if EOF('v_resultado')
-			
+
 			ins="INSERT INTO [COSEP_A].[dbo].[art](CO_ART,ART_DES,CO_LIN,CO_CAT,CO_SUBL,CO_COLOR,PROCEDENCI,CO_PROV,UNI_VENTA,"+;
 			" SUNI_VENTA,TIPO,TIPO_IMP,COMENTARIO,PORC_COS,cos_merc,prec_vta1,prec_vta2,prec_vta3,prec_vta4,prec_vta5, dis_cen, tipo_cos, campo1, campo2, campo8)"+;
 			" VALUES (?v_SC.co_art,?v_SC.art_des,?v_SC.co_lin,?v_SC.co_cat,?v_SC.co_subl,?v_SC.co_color,?v_SC.procedenci,"+;
@@ -249,14 +301,14 @@ do while !EOF('v_SC')
 			   Return .F.
 			else
 				conCOSEP=conCOSEP+1
-				*MESSAGEBOX("COSEP_A: SE INSERTO UN NUEVO ARTICULO: " + v_SC.co_art)	
+				*MESSAGEBOX("COSEP_A: SE INSERTO UN NUEVO ARTICULO: " + v_SC.co_art)
 			ENDIF
 		ENDIF
 
 	*CONAI_A
 		tresult1=sqlexec(tconnect1,'select * from CONAI_A.dbo.art where co_art=?v_SC.co_art','v_resultado')
 		if EOF('v_resultado')
-			
+
 			ins="INSERT INTO [CONAI_A].[dbo].[art](CO_ART,ART_DES,CO_LIN,CO_CAT,CO_SUBL,CO_COLOR,PROCEDENCI,CO_PROV,UNI_VENTA,"+;
 			" SUNI_VENTA,TIPO,TIPO_IMP,COMENTARIO,PORC_COS,cos_merc,prec_vta1,prec_vta2,prec_vta3,prec_vta4,prec_vta5, dis_cen, tipo_cos, campo1, campo2, campo8)"+;
 			" VALUES (?v_SC.co_art,?v_SC.art_des,?v_SC.co_lin,?v_SC.co_cat,?v_SC.co_subl,?v_SC.co_color,?v_SC.procedenci,"+;
@@ -269,14 +321,14 @@ do while !EOF('v_SC')
 			   Return .F.
 			else
 				conCONAI=conCONAI+1
-				*MESSAGEBOX("CONAI_A: SE INSERTO UN NUEVO ARTICULO: " + v_SC.co_art)	
+				*MESSAGEBOX("CONAI_A: SE INSERTO UN NUEVO ARTICULO: " + v_SC.co_art)
 			ENDIF
 		ENDIF
 
 	*FRIOS_A
 		tresult1=sqlexec(tconnect1,'select * from FRIOS_A.dbo.art where co_art=?v_SC.co_art','v_resultado')
 		if EOF('v_resultado')
-			
+
 			ins="INSERT INTO [FRIOS_A].[dbo].[art](CO_ART,ART_DES,CO_LIN,CO_CAT,CO_SUBL,CO_COLOR,PROCEDENCI,CO_PROV,UNI_VENTA,"+;
 			" SUNI_VENTA,TIPO,TIPO_IMP,COMENTARIO,PORC_COS,cos_merc,prec_vta1,prec_vta2,prec_vta3,prec_vta4,prec_vta5, dis_cen, tipo_cos, campo1, campo2, campo8)"+;
 			" VALUES (?v_SC.co_art,?v_SC.art_des,?v_SC.co_lin,?v_SC.co_cat,?v_SC.co_subl,?v_SC.co_color,?v_SC.procedenci,"+;
@@ -289,12 +341,11 @@ do while !EOF('v_SC')
 			   Return .F.
 			else
 				conFRIOS=conFRIOS+1
-				*MESSAGEBOX("FRIOS_A: SE INSERTO UN NUEVO ARTICULO: " + v_SC.co_art)	
+				*MESSAGEBOX("FRIOS_A: SE INSERTO UN NUEVO ARTICULO: " + v_SC.co_art)
 			ENDIF
 		ENDIF
 skip in v_SC
 enddo
-
 
 InsertResultString="Se han insertado la siguiente cantidad de articulos en las empresas."+Chr(13)+Chr(13)+"ART_A: "+ALLTRIM(STR(con))+Chr(13)+"COSEP_A: "+ALLTRIM(STR(conCOSEP))+Chr(13)+"CONAI_A: "+ALLTRIM(STR(conCONAI))+Chr(13)+"FRIOS_A: "+ALLTRIM(STR(conFRIOS))
 Messagebox(InsertResultString,64,"Insercion de Articulos Nuevos")
@@ -302,6 +353,7 @@ Messagebox(InsertResultString,64,"Insercion de Articulos Nuevos")
 *************************************************************************
 *ACTUALIZACION DE ARTICULOS*
 *************************************************************************
+
 *La actualizacion para ART_A es completa... Una replica de COSER_A
 *La actualizacion para COSEP_A se hace quitando el IVA y tomando en cuenta que el prec_vta3>0 y COSER_A.stock_act>0
 *La actualizacion para CONAI_A se hace siempre y cuando el cos_merc de COSER_A > CONAI_A y COSER_A.stock_act>0
@@ -312,9 +364,9 @@ Messagebox(InsertResultString,64,"Insercion de Articulos Nuevos")
 	*ART_A
 	act="update [ART_A].[DBO].[ART] set porc_cos=COSER_A.porc_cos, "+;
 	"stock_act=COSER_A.stock_act, prec_vta1=COSER_A.prec_vta1, prec_vta2=COSER_A.prec_vta2, prec_vta3=COSER_A.prec_vta3, prec_vta4=COSER_A.prec_vta4, prec_vta5=COSER_A.prec_vta5, "+;
-	"fec_prec_v=COSER_A.fec_prec_v, fec_prec_2=COSER_A.fec_prec_2, fec_prec_3=COSER_A.fec_prec_3, fec_prec_4=COSER_A.fec_prec_4, fec_prec_5=convert(smalldatetime,?fechaHoy,101),  " +; 
+	"fec_prec_v=COSER_A.fec_prec_v, fec_prec_2=COSER_A.fec_prec_2, fec_prec_3=COSER_A.fec_prec_3, fec_prec_4=COSER_A.fec_prec_4, fec_prec_5=convert(smalldatetime,?fechaHoy,101),  " +;
 	"fec_cos_p2=COSER_A.fec_cos_p2, cos_merc=COSER_A.cos_merc, fec_cos_me=COSER_A.fec_cos_me, tipo_cos=COSER_A.tipo_cos, dis_cen=COSER_A.dis_cen, ULT_COS_UN=COSER_A.ULT_COS_UN, fec_ult_co=COSER_A.fec_ult_co "+;
-	"FROM COSER_A.dbo.art "+; 
+	"FROM COSER_A.dbo.art "+;
 	"AS COSER_A inner join ART_A.dbo.art as ART_A on COSER_A.co_art=ART_A.co_art "
 
 	tresult3=sqlexec(tconnect3,act)
@@ -324,7 +376,7 @@ Messagebox(InsertResultString,64,"Insercion de Articulos Nuevos")
 
 	act="update [ART_A].[DBO].[ART] set art_des=COSER_A.art_des, comentario=COSER_A.comentario, "+;
 	"campo1=COSER_A.campo1, campo2=COSER_A.campo2, campo8=?fechaHora "+;
-	"FROM COSER_A.dbo.art "+; 
+	"FROM COSER_A.dbo.art "+;
 	"AS COSER_A inner join ART_A.dbo.art as ART_A on COSER_A.co_art=ART_A.co_art "
 
 	tresult4=sqlexec(tconnect4,act)
@@ -335,9 +387,9 @@ Messagebox(InsertResultString,64,"Insercion de Articulos Nuevos")
 	*COSEP_A
 	act="update [COSEP_A].[DBO].[ART] set porc_cos=((( (COSER_A.prec_vta3/'1.2544')-COSER_A.cos_merc)/(COSER_A.prec_vta3/'1.2544'))*100 -1), "+;
 	"prec_vta1=COSER_A.prec_vta1/'1.12', prec_vta2=COSER_A.prec_vta2/'1.12', prec_vta3=COSER_A.prec_vta3/'1.12', prec_vta4=COSER_A.prec_vta4/'1.12', prec_vta5=COSER_A.prec_vta5/'1.12', "+;
-	"fec_prec_v=COSER_A.fec_prec_v, fec_prec_2=COSER_A.fec_prec_2, fec_prec_3=COSER_A.fec_prec_3, fec_prec_4=COSER_A.fec_prec_4, fec_prec_5=convert(smalldatetime,?fechaHoy,101),  " +; 
+	"fec_prec_v=COSER_A.fec_prec_v, fec_prec_2=COSER_A.fec_prec_2, fec_prec_3=COSER_A.fec_prec_3, fec_prec_4=COSER_A.fec_prec_4, fec_prec_5=convert(smalldatetime,?fechaHoy,101),  " +;
 	"fec_cos_p2=COSER_A.fec_cos_p2, cos_merc=COSER_A.cos_merc, fec_cos_me=COSER_A.fec_cos_me, tipo_cos=COSER_A.tipo_cos, dis_cen=COSER_A.dis_cen, ULT_COS_UN=COSER_A.ULT_COS_UN, fec_ult_co=COSER_A.fec_ult_co "+;
-	"FROM COSER_A.dbo.art "+; 
+	"FROM COSER_A.dbo.art "+;
 	"AS COSER_A inner join COSEP_A.dbo.art as COSEP_A on COSER_A.co_art=COSEP_A.co_art " +;
 	"WHERE COSER_A.prec_vta3>0"
 
@@ -348,7 +400,7 @@ Messagebox(InsertResultString,64,"Insercion de Articulos Nuevos")
 
 	act="update [COSEP_A].[DBO].[ART] set art_des=COSER_A.art_des, comentario=COSER_A.comentario, "+;
 	"campo1=COSER_A.campo1, campo2=COSER_A.campo2, campo8=?fechaHora "+;
-	"FROM COSER_A.dbo.art "+; 
+	"FROM COSER_A.dbo.art "+;
 	"AS COSER_A inner join COSEP_A.dbo.art as COSEP_A on COSER_A.co_art=COSEP_A.co_art "
 
 	tresult4=sqlexec(tconnect4,act)
@@ -360,9 +412,9 @@ Messagebox(InsertResultString,64,"Insercion de Articulos Nuevos")
 	*CONAI_A
 	act="update [CONAI_A].[DBO].[ART] set porc_cos=COSER_A.porc_cos, "+;
 	"prec_vta1=COSER_A.prec_vta1, prec_vta2=COSER_A.prec_vta2, prec_vta3=COSER_A.prec_vta3, prec_vta4=COSER_A.prec_vta4, prec_vta5=COSER_A.prec_vta5, "+;
-	"fec_prec_v=COSER_A.fec_prec_v, fec_prec_2=COSER_A.fec_prec_2, fec_prec_3=COSER_A.fec_prec_3, fec_prec_4=COSER_A.fec_prec_4, fec_prec_5=convert(smalldatetime,?fechaHoy,101), " +; 
+	"fec_prec_v=COSER_A.fec_prec_v, fec_prec_2=COSER_A.fec_prec_2, fec_prec_3=COSER_A.fec_prec_3, fec_prec_4=COSER_A.fec_prec_4, fec_prec_5=convert(smalldatetime,?fechaHoy,101), " +;
 	"fec_cos_p2=COSER_A.fec_cos_p2, cos_merc=COSER_A.cos_merc, fec_cos_me=COSER_A.fec_cos_me, tipo_cos=COSER_A.tipo_cos, dis_cen=COSER_A.dis_cen "+;
-	"FROM COSER_A.dbo.art "+; 
+	"FROM COSER_A.dbo.art "+;
 	"AS COSER_A inner join CONAI_A.dbo.art as CONAI_A on COSER_A.co_art=CONAI_A.co_art " +;
 	"WHERE COSER_A.stock_act> 0 AND (COSER_A.cos_merc > CONAI_A.cos_merc)"
 
@@ -373,7 +425,7 @@ Messagebox(InsertResultString,64,"Insercion de Articulos Nuevos")
 
 	act="update [CONAI_A].[DBO].[ART] set art_des=COSER_A.art_des, comentario=COSER_A.comentario, "+;
 	"campo1=COSER_A.campo1, campo2=COSER_A.campo2, campo8=?fechaHora, ULT_COS_UN=COSER_A.ULT_COS_UN, fec_ult_co=COSER_A.fec_ult_co "+;
-	"FROM COSER_A.dbo.art "+; 
+	"FROM COSER_A.dbo.art "+;
 	"AS COSER_A inner join CONAI_A.dbo.art as CONAI_A on COSER_A.co_art=CONAI_A.co_art "
 
 	tresult4=sqlexec(tconnect4,act)
@@ -384,9 +436,9 @@ Messagebox(InsertResultString,64,"Insercion de Articulos Nuevos")
 	*FRIOS_A
 	act="update [FRIOS_A].[DBO].[ART] set porc_cos=COSER_A.porc_cos, "+;
 	"prec_vta1=COSER_A.prec_vta1, prec_vta2=COSER_A.prec_vta2, prec_vta3=COSER_A.prec_vta3, prec_vta4=COSER_A.prec_vta4, prec_vta5=COSER_A.prec_vta5, "+;
-	"fec_prec_v=COSER_A.fec_prec_v, fec_prec_2=COSER_A.fec_prec_2, fec_prec_3=COSER_A.fec_prec_3, fec_prec_4=COSER_A.fec_prec_4, fec_prec_5=convert(smalldatetime,?fechaHoy,101), " +; 
+	"fec_prec_v=COSER_A.fec_prec_v, fec_prec_2=COSER_A.fec_prec_2, fec_prec_3=COSER_A.fec_prec_3, fec_prec_4=COSER_A.fec_prec_4, fec_prec_5=convert(smalldatetime,?fechaHoy,101), " +;
 	"fec_cos_p2=COSER_A.fec_cos_p2, cos_merc=COSER_A.cos_merc, fec_cos_me=COSER_A.fec_cos_me, tipo_cos=COSER_A.tipo_cos, dis_cen=COSER_A.dis_cen "+;
-	"FROM COSER_A.dbo.art "+; 
+	"FROM COSER_A.dbo.art "+;
 	"AS COSER_A inner join FRIOS_A.dbo.art as FRIOS_A on COSER_A.co_art=FRIOS_A.co_art " +;
 	"WHERE COSER_A.stock_act> 0 AND (COSER_A.cos_merc > FRIOS_A.cos_merc)"
 
@@ -397,7 +449,7 @@ Messagebox(InsertResultString,64,"Insercion de Articulos Nuevos")
 
 	act="update [FRIOS_A].[DBO].[ART] set art_des=COSER_A.art_des, comentario=COSER_A.comentario, "+;
 	"campo1=COSER_A.campo1, campo2=COSER_A.campo2, campo8=?fechaHora, ULT_COS_UN=COSER_A.ULT_COS_UN, fec_ult_co=COSER_A.fec_ult_co "+;
-	"FROM COSER_A.dbo.art "+; 
+	"FROM COSER_A.dbo.art "+;
 	"AS COSER_A inner join FRIOS_A.dbo.art as FRIOS_A on COSER_A.co_art=FRIOS_A.co_art "
 
 	tresult4=sqlexec(tconnect4,act)
@@ -408,34 +460,3 @@ Messagebox(InsertResultString,64,"Insercion de Articulos Nuevos")
 
 
 MESSAGEBOX(":.:Proceso Actualizacion De Precios :.: "+Chr(13)+"--> Completado Exitosamente <--" +Chr(13)+"Recuerde actualizar las Tablas locales en las empresas actualizadas.",64,"::Dpto Informatica :) Compresores Servicios::")
-
-*************************************************************************
-
-*************************************************************************
-*ACTUALIZADO EL 26-05-2014
-*ACTUALIZA TARIBA LLEVANDO TODOS LOS DATOS DE LOS ARTICULOS DE BARRIO OBRERO...
-*ACTUALIZA COSEP_A = QUE TARIBA PERO LE QUITA EL IVA A LOS PRECIOS Y POR SUPUESTO AL MARGEN MINIMO...
-*USA CAMPOS 7 Y 8 PARA GUARDAR LA FECHA Y LA HORA DE ACTUALIZACION... TANTO EN COSEP_A COMO EN TARIBA
-*LOS ARTICULOS QUE NO TIENEN PRECIOS... NO SE ACTUALIZAN DE NINGUNA FORMA... POR ELLO PUEDE HABER ARTICULOS QUE NO TENGAN LA FECHA DE ACTUALIZACION...
-*************************************************************************
-*** ACTUALIZADO EL 15-01-15 		*
-*** ACTUALIZA: SOL_A, SOLP_A, COSEP_A, CONAI_A				*
-*** EL PROCESO CAMBIO DEBIDO A QUE SOL_A Y CONAI_A HACEN COMPRAS ENTONCES	*
-*** ESTA ACTUALIZACION DE DATA SE HACE UNICAMENTE CUANDO  U *
-*** COSER_A.ART.COS_MERC > EMPRESAXXX.ART.ULT_COS_UN *
-*** YA QUE SIEMPRE SE DEBEN MANTENER LOS PRECIOS/COSTOS MAS ALTOS *
-*************************************************************************
-*** Actualizado 18-01-15
-*** Sacamos a SOL Y SOLP y el proceso se divide en 2.
-*** Este que lo ejecuta Wender
-*** Y otro que lo ejecutara Duvin. (muy parecido al que corria JeanCarlos)
-*** Aqui se setean las horas en las cuales el proceso se ejecuto por Wender... esta informacion se repartira a las demas empresas para referencias... 
-*** LA CONDICION DE PREC_VTA3 en el update NO ES NECESARIA LOGICAMENTE PERO ESTA PARA EVITAR ERRORES DEL PERSONAL DE COSERVICA
-*** Si hay cambios en las descripciones las mismas no se haran sino se cambia un costo y se cumpla la condicion de que el costo sea mayor
-*************************************************************************
-*** ACtualizado 20-01-15
-*** Agrega campo2 que sera el codigo antiguo de REfrigeracion El Sol.
-*************************************************************************
-*** 02/25/16
-*** Se Agrega stock_act a ART_A
-*************************************************************************
